d5cf1b86a37bc5be9b216338e592e4ecd27c7b1a
sharlinu <sharlinutke@gmail.com>
2023-09-06 14:08:46 +0100

    adds MAAC functionalities to utils

--- utils/buffer.py
+++ utils/buffer.py
@@ -3,19 +3,11 @@ from torch import Tensor
 from torch.autograd import Variable
 from collections import deque
 
-class ReplayBuffer(object):
+class ReplayBufferMAAC(object):
     """
     Replay Buffer for multi-agent RL with parallel rollouts
     """
-
-    def __init__(self,
-                 max_steps,
-                 num_agents,
-                 obs_dims,
-                 # nullary_dims,
-                 unary_dims,
-                 # binary_dims,
-                 ac_dims):
+    def __init__(self, max_steps, num_agents, obs_dims, ac_dims):
         """
         Inputs:
             max_steps (int): Maximum number of timepoints to store in buffer
@@ -27,93 +19,51 @@ class ReplayBuffer(object):
         self.max_steps = max_steps
         self.num_agents = num_agents
         self.obs_buffs = []
-        # self.obs_nullary_buffs = []
-        self.obs_unary_buffs = []
-        # self.obs_binary_buffs = []
         self.ac_buffs = []
         self.rew_buffs = []
         self.next_obs_buffs = []
-        self.next_obs_unary_buffs = []
         self.done_buffs = []
-        for odim, unary_dim, adim in zip(obs_dims, unary_dims, ac_dims):
+        for odim, adim in zip(obs_dims, ac_dims):
             self.obs_buffs.append(np.zeros((max_steps, odim), dtype=np.float32))
-            # self.obs_nullary_buffs.append(np.zeros((max_steps, nullary_dim), dtype=np.float32))
-            self.obs_unary_buffs.append(np.zeros((max_steps,) + unary_dim, dtype=np.float32))
-            # self.obs_binary_buffs.append(np.zeros((max_steps,) + binary_dim, dtype=np.float32))
-
             self.ac_buffs.append(np.zeros((max_steps, adim), dtype=np.float32))
-
             self.rew_buffs.append(np.zeros(max_steps, dtype=np.float32))
-
             self.next_obs_buffs.append(np.zeros((max_steps, odim), dtype=np.float32))
-            # self.next_obs_nullary_buffs.append(np.zeros((max_steps, nullary_dim), dtype=np.float32)) # this needs to change if we ever have a nullary tensor
-            self.next_obs_unary_buffs.append(np.zeros((max_steps,) + unary_dim, dtype=np.float32))
-            # self.next_obs_binary_buffs.append(np.zeros((max_steps,) + binary_dim, dtype=np.float32))
-
             self.done_buffs.append(np.zeros(max_steps, dtype=np.uint8))
 
+
         self.filled_i = 0  # index of first empty location in buffer (last index when full)
-        self.curr_i = 0  # current index to write to (overwrite oldest data)
+        self.curr_i = 0  # current index to write to (ovewrite oldest data)
 
     def __len__(self):
         return self.filled_i
 
-    def push(self,
-             observation: tuple,
-             actions: list,
-             rewards,
-             next_observation: tuple,
-             dones):
-        nentries = 1    # usually for multiple parallel environments, but now just one anyways
-
-        # TODO these are all missing the dimension in the beginning compared to parallel env set up of maac. e.g. obs (12,) instead of (1,2,12)
+    def push(self, observations, actions, rewards, next_observations, dones):
+        nentries = observations.shape[0]  # handle multiple parallel environments
         if self.curr_i + nentries > self.max_steps:
-            rollover = self.max_steps - self.curr_i  # num of indices to roll over
+            rollover = self.max_steps - self.curr_i # num of indices to roll over
             for agent_i in range(self.num_agents):
                 self.obs_buffs[agent_i] = np.roll(self.obs_buffs[agent_i],
                                                   rollover, axis=0)
-                # self.obs_nullary_buffs[agent_i] = np.roll(self.obs_nullary_buffs[agent_i],
-                #                                   rollover, axis=0)
-                self.obs_unary_buffs[agent_i] = np.roll(self.obs_unary_buffs[agent_i],
-                                                  rollover, axis=0)
-                # self.obs_binary_buffs[agent_i] = np.roll(self.obs_binary_buffs[agent_i],
-                #                                   rollover, axis=0)
                 self.ac_buffs[agent_i] = np.roll(self.ac_buffs[agent_i],
                                                  rollover, axis=0)
                 self.rew_buffs[agent_i] = np.roll(self.rew_buffs[agent_i],
                                                   rollover)
-
                 self.next_obs_buffs[agent_i] = np.roll(
                     self.next_obs_buffs[agent_i], rollover, axis=0)
-                # self.next_obs_nullary_buffs[agent_i] = np.roll(
-                #     self.next_obs_nullary_buffs[agent_i], rollover, axis=0)
-                self.next_obs_unary_buffs[agent_i] = np.roll(
-                    self.next_obs_unary_buffs[agent_i], rollover, axis=0)
-                # self.next_obs_binary_buffs[agent_i] = np.roll(
-                #     self.next_obs_binary_buffs[agent_i], rollover, axis=0)
-
                 self.done_buffs[agent_i] = np.roll(self.done_buffs[agent_i],
                                                    rollover)
             self.curr_i = 0
             self.filled_i = self.max_steps
-        for agent_i in range(self.num_agents):
-            self.obs_buffs[agent_i][self.curr_i:self.curr_i + nentries] = observation[agent_i]['image'].flatten()
-            # self.obs_nullary_buffs[agent_i][self.curr_i:self.curr_i + nentries] = observation[agent_i]['nullary']
-            self.obs_unary_buffs[agent_i][self.curr_i:self.curr_i + nentries] = observation[agent_i]['unary_tensor']
-            # self.obs_binary_buffs[agent_i][self.curr_i:self.curr_i + nentries] = observation[agent_i]['binary_tensor']
-
+        for agent_i in range(self.num_agents):\
+            # first index agent, 2nd index replay_length
+            self.obs_buffs[agent_i][self.curr_i:self.curr_i + nentries] = np.vstack(
+                observations[:, agent_i]) #np.vstack not needed with just one environment
+            # actions are already batched by agent, so they are indexed differently
             self.ac_buffs[agent_i][self.curr_i:self.curr_i + nentries] = actions[agent_i]
-            self.rew_buffs[agent_i][self.curr_i:self.curr_i + nentries] = rewards[agent_i]
-
-            self.next_obs_buffs[agent_i][self.curr_i:self.curr_i + nentries] = next_observation[agent_i]['image'].flatten()
-            # self.next_obs_nullary_buffs[agent_i][self.curr_i:self.curr_i + nentries] = next_observation[agent_i]['nullary']
-            try:
-                self.next_obs_unary_buffs[agent_i][self.curr_i:self.curr_i + nentries] = next_observation[agent_i]['unary_tensor']
-            except:
-                print('Number of objects in observation changes')
-            # self.next_obs_binary_buffs[agent_i][self.curr_i:self.curr_i + nentries] = next_observation[agent_i]['binary_tensor']
-
-            self.done_buffs[agent_i][self.curr_i:self.curr_i + nentries] = dones[agent_i]
+            self.rew_buffs[agent_i][self.curr_i:self.curr_i + nentries] = rewards[:, agent_i]
+            self.next_obs_buffs[agent_i][self.curr_i:self.curr_i + nentries] = np.vstack(
+                next_observations[:, agent_i])
+            self.done_buffs[agent_i][self.curr_i:self.curr_i + nentries] = dones[:, agent_i]
         self.curr_i += nentries
         if self.filled_i < self.max_steps:
             self.filled_i += nentries
@@ -123,28 +73,26 @@ class ReplayBuffer(object):
     def sample(self, N, to_gpu=False, norm_rews=True):
         inds = np.random.choice(np.arange(self.filled_i), size=N,
                                 replace=True)
+        # all this does is turn sample into a tensor
         if to_gpu:
             cast = lambda x: Variable(Tensor(x), requires_grad=False).cuda()
         else:
             cast = lambda x: Variable(Tensor(x), requires_grad=False)
-        if norm_rews:
+        if norm_rews and any([self.rew_buffs[i][:self.filled_i].mean() for i in range(self.num_agents)]) !=0 :
             ret_rews = [cast((self.rew_buffs[i][inds] -
                               self.rew_buffs[i][:self.filled_i].mean()) /
-                             self.rew_buffs[i][:self.filled_i].std()) if self.rew_buffs[i][:self.filled_i].mean() != 0
-                        else cast(self.rew_buffs[i][inds])
-                        for i in range(self.num_agents)]
+                             self.rew_buffs[i][:self.filled_i].std())
+                        if self.rew_buffs[i][:self.filled_i].mean() !=0
+                        else cast(self.rew_buffs[i][inds]) for i in range(self.num_agents)]
+            # print('ret_rews', ret_rews)
+            # print('mean, std 0', self.rew_buffs[0][:self.filled_i].mean(), self.rew_buffs[0][:self.filled_i].std())
+            # print('mean, std 1', self.rew_buffs[1][:self.filled_i].mean(), self.rew_buffs[1][:self.filled_i].std())
         else:
             ret_rews = [cast(self.rew_buffs[i][inds]) for i in range(self.num_agents)]
         return ([cast(self.obs_buffs[i][inds]) for i in range(self.num_agents)],
-                # [cast(self.obs_nullary_buffs[i][inds]) for i in range(self.num_agents)],
-                [cast(self.obs_unary_buffs[i][inds]) for i in range(self.num_agents)],
-                # [cast(self.obs_binary_buffs[i][inds]) for i in range(self.num_agents)],
                 [cast(self.ac_buffs[i][inds]) for i in range(self.num_agents)],
                 ret_rews,
                 [cast(self.next_obs_buffs[i][inds]) for i in range(self.num_agents)],
-                # [cast(self.next_obs_nullary_buffs[i][inds]) for i in range(self.num_agents)],
-                [cast(self.next_obs_unary_buffs[i][inds]) for i in range(self.num_agents)],
-                # [cast(self.next_obs_binary_buffs[i][inds]) for i in range(self.num_agents)],
                 [cast(self.done_buffs[i][inds]) for i in range(self.num_agents)])
 
     def get_average_rewards(self, N):
@@ -155,9 +103,9 @@ class ReplayBuffer(object):
         return [self.rew_buffs[i][inds].mean() for i in range(self.num_agents)]
 
 
-class ReplayBuffer2(object):
+class ReplayBufferMARC(object):
     """
-    Replay Buffer for multi-agent RL with parallel rollouts
+    Replay Buffer for multi-agent RL with stored graph data
     """
 
     def __init__(self,
